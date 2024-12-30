837f35c2b956c5535ac77ff84b1b8eed3e777dd0
Andreas Söderlund <ciscoheat@gmail.com>
2024-12-30 08:17:36 +0100

    Fixed book dragging style

--- src/lib/assets/book/Book.svelte
+++ src/lib/assets/book/Book.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
+	//import type { DropEvent } from '@interactjs/actions/drop/DropEvent';
+	import type { DragEvent } from '@interactjs/actions/drag/plugin';
 	import type { Book } from '$lib/models/book';
 	import { world } from '$lib/assets/world';
 
 	let { book }: { book: Book } = $props();
 	let pos = $state({ x: 0, y: 0 });
+
+	function onstart(e: DragEvent) {
+		e.target.classList.remove('shelved');
+	}
+
+	function onend(e: DragEvent) {
+		if (e.relatedTarget?.id !== 'bookshelf') return;
+		e.target.classList.add('shelved');
+	}
 </script>
 
 <div
 	class="book shelved"
 	style="--thickness: {Math.max(35, book.pages / 15)}px; --x: {pos.x}px; --y: {pos.y}px"
-	use:world.draggable={{ object: book, pos }}
+	use:world.draggable={{ object: book, pos, onend, onstart }}
 >
 	{book.title}
 </div>
@@ -25,13 +36,13 @@
 		touch-action: none;
 		user-select: none;
 		z-index: 3;
-		transform: rotate(-2deg);
+		transform: translate(calc(var(--x) + 6px), calc(var(--y) - 2px)) rotate(-2deg);
 
 		&.shelved {
+			transform: translate(calc(var(--x) + 6px), calc(var(--y) - 2px)) rotate(175deg);
 			height: unset;
 			max-height: 170px;
 			line-height: 1.25em;
-			transform: translate(calc(var(--x) + 6px), calc(var(--y) - 2px)) rotate(175deg);
 			writing-mode: vertical-rl;
 			display: flex;
 			min-width: var(--thickness);
