b02743429776cb05cf0aef1ff76c0c3471b6e2f7
Andreas Söderlund <ciscoheat@gmail.com>
2025-01-06 08:33:30 +0100

    ExpectedError updated

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,8 +1,12 @@
 // Thanks to https://stackoverflow.com/a/48342359/70894
 export class ExpectedError extends Error {
-	constructor(message: string) {
+	constructor(
+		message: string,
+		public readonly code?: string | number,
+		options?: ErrorOptions
+	) {
 		// 'Error' breaks prototype chain here
-		super(message);
+		super(message, options);
 
 		// restore prototype chain
 		Object.setPrototypeOf(this, new.target.prototype);
